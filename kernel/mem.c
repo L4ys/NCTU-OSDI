f81079d8ba718968d276ad9f2a99094dc2b53583
Lays <poca0725@gmail.com>
2017-04-12 05:57:58 +0800

    Allocate memory for pages

--- kernel/mem.c
+++ kernel/mem.c
@@ -146,7 +146,8 @@ mem_init(void)
 	// array.  'npages' is the number of physical pages in memory.  Use memset
 	// to initialize all fields of each struct PageInfo to 0.
 	// Your code goes here:
-    /* TODO */
+    pages = (struct PageInfo*)boot_alloc(sizeof(struct PageInfo) * npages);
+    memset(pages, 0, sizeof(struct PageInfo) * npages);
 
 	//////////////////////////////////////////////////////////////////////
 	// Now that we've allocated the initial kernel data structures, we set
